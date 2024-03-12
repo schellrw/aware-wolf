1ac6c6945b7fb8f76d0d5d86b50b203c94b0828a
schellrw <schell.rw@gmail.com>
2024-03-11 22:59:00 -0500

    bite off less

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ API_URL = f"https://api-inference.huggingface.co/models/{st.secrets.MODEL}"
 @st.cache_data
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+    output = response.json()
+    output = output['summaries'][0]    
+    return output
+    #return response.json()
 
 # Add title and subtitle to the main interface of the app
 st.title("Aware Wolf: Service Update Scanner")
@@ -24,13 +27,13 @@ st.sidebar.markdown("##### :gray[All rights reserved.]")
 # Add two columns to the app
 col1, col2 = st.columns(2)
 with col1:
-    text_input = st.text_area("Paste that update word salad below and activate the Aware Wolf...", height=200)
-    context = "\n\n#### Please summarize this dense legalese, regarding updates to service terms and data privacy, into simple terms that laypeople can understand. ###"
+    text_input = st.text_area("Paste that update word salad below and activate AWARE WOLF...", height=200)
+    context = "\n\n#### Please summarize this dense language into simple terms that ordinary people can understand, in 828 tokens, or less. ###"
     # focus = "\n\n## Please be sure to highlight any peculiar changes and/or odd data handling practices, including, but not limited to third party handling and potential ways to protect your data. ##\n\n"
     # sale = "\n\n# Please suggest any potential open source alternatives to the software described in the request that the user may be able to use instead. #\n\n"
     text_input = text_input + context # + focus + sale
 with col2:
-    submit = st.button("Activate the Aware Wolf")
+    submit = st.button("Activate AWARE WOLF!")
     if text_input and submit:
         out = query({"inputs": text_input})
         out = st.write(out, height=200, label="Summary:")
